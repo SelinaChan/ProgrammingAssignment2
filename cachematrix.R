5550b337db902146987ba2a6da6e2580d19db98d
Bing <Bing@WillChen.lan>
2017-06-11 16:39:26 +0800

    correct the setinverse function and add some comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,6 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caching the Inverse of a Matrix
 
 ## This function creates a special "matrix" object that can cache its inverse.
-
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
         set <- function(y){
@@ -10,7 +8,7 @@ makeCacheMatrix <- function(x = matrix()) {
                 m <<- NULL
         }
         get <- function() x
-        setinverse <- function(inverse) m <<- inverse
+        setinverse <- function(solve) m <<- solve #solve return an inverse of a matrix
         getinverse <- function() m
         list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
@@ -19,7 +17,6 @@ makeCacheMatrix <- function(x = matrix()) {
 ## acheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
 ## If the inverse has already been calculated (and the matrix has not changed), 
 ## then the cachesolve should retrieve the inverse from the cache.
-
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
@@ -32,5 +29,4 @@ cacheSolve <- function(x, ...) {
         m <- solve(data, ...)
         x$setinverse(m)
         m
-        
 }
